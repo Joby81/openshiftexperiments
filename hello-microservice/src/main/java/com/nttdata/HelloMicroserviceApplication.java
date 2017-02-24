c893e8ab1fa6e1ec78ca972c75e3f9bb09390424
POOPPJ <POOPPJ@PC46870.de.softlab.net>
2017-02-24 09:28:05 +0100

    adding restcontroller

--- hello-microservice/src/main/java/com/nttdata/HelloMicroserviceApplication.java
+++ hello-microservice/src/main/java/com/nttdata/HelloMicroserviceApplication.java
@@ -2,11 +2,19 @@ package com.nttdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloMicroserviceApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String sayHello() {
+		return ("Hello, SpringBoot on Wildfly created by Joby");
 	}
 }
